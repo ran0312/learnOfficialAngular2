a69387c4d79d5d98fc470e9662626a5b7a1f75fa
Rannie <rannie123.liu@gmail.com>
2018-07-25 18:56:52 +0800

    服务

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,10 +12,15 @@ export class HeroesComponent implements OnInit {
   onSelect(hero:Hero):void{
     this.selectedHero = hero;
   }
-  heroes = HEROES;
-  constructor() { }
+  heroes: Hero[];
+  constructor(private heroService:HeroService) { }
+  getHeroes():void{
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
